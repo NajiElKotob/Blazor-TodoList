97abd31bc564094c390dfa37892012d9fed55348
najielkotob <naji@dotnetheroes.com>
2021-10-21 13:11:53 +0400

    Add RemoveTodo feature

--- TodoList/Classes/TodoItem.cs
+++ TodoList/Classes/TodoItem.cs
@@ -1,8 +1,12 @@
-﻿namespace TodoList.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Classes
 {
     public class TodoItem
     {
         public Guid Id { get; set; }
+
+        [StringLength(20)]
         public string Title { get; set; }
         public bool IsDone { get; set; }
     }
