bc40b368f566f6217def4684d281109b0c8f6cd7
najielkotob <naji@dotnetheroes.com>
2023-10-22 23:34:16 +0300

    Initiate a .NET MAUI App 'TodoListMauiApp' with default configurations.

---
+++ TodoListMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TodoListMauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
