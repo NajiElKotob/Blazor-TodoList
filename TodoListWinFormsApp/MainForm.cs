f059b8d75fdaadea64c4490c15ec206e09a3a3db
najielkotob <naji@dotnetheroes.com>
2023-10-24 22:49:34 +0300

    Miscellaneous tweaks and enhancements

--- TodoListWinFormsApp/MainForm.cs
+++ TodoListWinFormsApp/MainForm.cs
@@ -1,8 +1,8 @@
 namespace TodoListWinFormsApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
