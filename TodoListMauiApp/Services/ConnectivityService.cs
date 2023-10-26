f204b39243f9ac3bceaa2846dc7f1716bfce7e3e
najielkotob <naji@dotnetheroes.com>
2023-10-26 15:24:43 +0300

    General improvements and bug fixes

--- TodoListMauiApp/Services/ConnectivityService.cs
+++ TodoListMauiApp/Services/ConnectivityService.cs
@@ -1,22 +1,40 @@
 ﻿namespace TodoListMauiApp.Services;
 
-
+/// <summary>
+/// Provides services related to connectivity status and events.
+/// </summary>
 public class ConnectivityService
 {
+    /// <summary>
+    /// Gets a value indicating whether the device is connected to the Internet.
+    /// </summary>
     public bool IsConnected => Connectivity.NetworkAccess == NetworkAccess.Internet;
 
+    /// <summary>
+    /// Event raised when connectivity status changes.
+    /// </summary>
     public event EventHandler<ConnectivityChangedEventArgs> ConnectivityChanged;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConnectivityService"/> class.
+    /// Subscribes to the system's connectivity changed event.
+    /// </summary>
     public ConnectivityService()
     {
         Connectivity.ConnectivityChanged += OnConnectivityChanged;
     }
 
+    /// <summary>
+    /// Handles the system's connectivity changed event and raises the <see cref="ConnectivityChanged"/> event.
+    /// </summary>
     private void OnConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
     {
         ConnectivityChanged?.Invoke(this, e);
     }
 
+    /// <summary>
+    /// Unsubscribes from the system's connectivity changed event to avoid memory leaks.
+    /// </summary>
     public void Dispose()
     {
         Connectivity.ConnectivityChanged -= OnConnectivityChanged;
--- TodoListMauiApp/Services/TodoService.cs
+++ TodoListMauiApp/Services/TodoService.cs
@@ -3,18 +3,30 @@ using TodoListMauiApp.Models;
 
 namespace TodoListMauiApp.Services;
 
+/// <summary>
+/// Provides methods to interact with the Todo API.
+/// </summary>
 public class TodoService
 {
+    // HTTP client to communicate with the Todo API.
     private readonly HttpClient client;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TodoService"/> class.
+    /// </summary>
     public TodoService()
     {
         client = new HttpClient
         {
+            // Base URL of the Todo API.
             BaseAddress = new Uri("https://localhost:5099/")
         };
     }
 
+    /// <summary>
+    /// Loads the list of Todo items from the API.
+    /// </summary>
+    /// <returns>A list of Todo items or null if an error occurs.</returns>
     public async Task<List<TodoItem>> LoadTodosAsync()
     {
         var response = await client.GetAsync("todos");
@@ -23,8 +35,11 @@ public class TodoService
             var json = await response.Content.ReadAsStringAsync();
             return JsonSerializer.Deserialize<List<TodoItem>>(json);
         }
-        return null; // Or throw an exception
+
+        // Returns null if there's an issue with the API response.
+        // Consider throwing an exception for better error handling.
+        return null;
     }
 
-    // ToDo: Implement AddTodo, RemoveTodo, ... methods 
+    // ToDo: Additional methods for adding, removing, updating, etc., to be implemented.
 }
